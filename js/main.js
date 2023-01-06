535b6b9c8b610f9371c23153fe14810207a4482b
niederhausertomas <78279996+niederhausertomas@users.noreply.github.com>
2023-01-06 14:29:58 -0300

    responsive

--- js/main.js
+++ js/main.js
@@ -112,3 +112,22 @@ function flechas(){
         flecha14.style.marginLeft = '60px';
         flecha14.style.transition = '12s';
         })
+//Formulario de contacto. formspree.io
+    const $form = document.querySelector('#form')
+    $form.addEventListener('submit', handleSubmit)
+
+    async function handleSubmit(event){
+        event.preventDefault()
+        const form = new FormData(this)
+        const response = await fetch(this.action, {
+            method: this.method,
+            body: form,
+            headers:{
+                'Accept': 'application/json'
+            }
+        })
+        if(response.ok){
+            this.reset();
+            alert("Mensaje enviado!")
+        }
+    }
